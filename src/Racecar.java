dcd0f7fd01d269014e21c76bc5b291f080c3e9ee
wotnamed <sjogrenalvin@icloud.com>
2026-03-13 11:12:28 +0100

    Racecar updates

--- src/Racecar.java
+++ src/Racecar.java
@@ -1,31 +1,45 @@
 public class Racecar implements PhysicsBasedVehicle, Vehicle {
+    private int[] previousVector;
+    private int thrustForce;
+    private int mass;
+    private int[] colour;
+    private int facingAngle;
+    private int[] coordinates;
+
+    public Racecar(int[] colour, int facingAngle, int thrustForce, int[] coordinates, int mass){
+        this.colour = colour;
+        this.facingAngle = facingAngle;
+        this.thrustForce = thrustForce;
+        this.coordinates = coordinates;
+        this.mass = mass;
+    }
     @Override
     public int[] getPreviousForceVector() {
-        return new int[0];
+        return previousVector;
     }
 
     @Override
     public int getThrustForce() {
-        return 0;
+        return thrustForce;
     }
 
     @Override
     public int getMass() {
-        return 0;
+        return mass;
     }
 
     @Override
     public int[] getCoordinates() {
-        return new int[0];
+        return coordinates;
     }
 
     @Override
     public int[] getColour() {
-        return new int[0];
+        return colour;
     }
 
     @Override
     public int getFacingAngle() {
-        return 0;
+        return facingAngle;
     }
 }
